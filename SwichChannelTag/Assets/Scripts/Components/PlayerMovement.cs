6e28553fbe5c6141461acf0283599af734a3e989
soltshio <shanshanshien@gmail.com>
2025-08-07 18:25:53 +0900

    MoveOnMapのコード整理
    MoveOnMapを純粋クラス(MonoBehaviourを継承していないクラス)にし、PlayerMovementコンポーネントにMoveOnMapを所有させるようにしました。

---
+++ SwichChannelTag/Assets/Scripts/Components/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.EditorTools;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+//作成者:杉山
+//キャラの動くコンポーネント(プロトタイプ段階)
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Tooltip("マップ上の移動関係")] [SerializeField] MoveOnMap _moveOnMap;
+
+    void Start()
+    {
+        _moveOnMap.Start();
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+
+        Vector2 getVec = context.ReadValue<Vector2>();
+
+        Debug.Log(getVec);
+
+        _moveOnMap.Move(getVec);
+    }
+}
---
+++ SwichChannelTag/Assets/Scripts/PureC#/Move/MoveOnMap.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//作成者:杉山
+//マップ上を移動する
+
+[System.Serializable]
+public class MoveOnMap
+{
+    [Tooltip("どのマップ上を動くか")] [SerializeField] Map_A_Hierarchy _map;
+    [Tooltip("動かす対象")] [SerializeField] Transform _target;
+    [Tooltip("初期位置")][SerializeField] MapVec _startPoint;
+
+    private MapVec _currentPos;
+
+    public Map_A_Hierarchy Map//どのマップ上を動くか
+    {
+        get { return _map; }
+        set { _map = value; }
+    }
+
+    public Transform Target//動かす対象
+    {
+        get { return _target; }
+        set { _target = value; }
+    }
+
+    public void Start()//Start関数で呼び出す
+    {
+        //位置の初期化
+        RewritePos(_startPoint);
+    }
+
+    public void Move(Vector2 inputVec)//移動
+    {
+        MapVec moveVec;
+        moveVec.x = (int)inputVec.x;
+        moveVec.y = (int)inputVec.y;
+
+        RewritePos(_currentPos + moveVec);
+    }
+
+    void RewritePos(MapVec newMapVec)//位置の書き換え
+    {
+        _currentPos = _map.ClampInRange(newMapVec);//範囲外の位置に行かないようにするための処置
+        Vector3 newPos;
+
+        _map.Transit_FromMapVec_ToWorldVec(_currentPos, out newPos);
+        _target.position = newPos;
+    }
+}
