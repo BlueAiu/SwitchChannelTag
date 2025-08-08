ca394a9225075611e0da2da5149b62a0478602fe
BlueAiu <aiujustitia@yahoo.co.jp>
2025-08-08 21:14:16 +0900

    生成の際プレイヤーの位置をずらすようにした
    現時点ではまだプレイヤーが抜けるときは想定していない

--- SwichChannelTag/Assets/Scripts/PunConecction/CreatePlayer.cs
+++ SwichChannelTag/Assets/Scripts/PunConecction/CreatePlayer.cs
@@ -6,13 +6,16 @@ using Photon.Realtime;
 public class CreatePlayer : MonoBehaviourPunCallbacks
 {
     [SerializeField] GameObject playerPrefab;
+    [SerializeField] Transform WaitingPoint;
+    [SerializeField] Vector3 shift;
+
+    const int masterNum = 1;
 
     public override void OnJoinedRoom()
     {
         var player = PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity);
 
-        // ‚±‚ÌŒãplayer‚ÌComponent‚ğ•ÒW‚Å‚«‚é
-
-        Debug.Log(player.GetComponent<PlayerNumber>().PlayerNum);
+        int playerNum = player.GetComponent<PlayerNumber>().PlayerNum;
+        player.transform.position = WaitingPoint.position + (playerNum - masterNum) * shift;
     }
 }
