55dcc4749d2591839daf6b29c17ee19c4ef90d2a
BlueAiu <aiujustitia@yahoo.co.jp>
2025-08-14 23:13:07 +0900

    参加しているプレイヤー達を管理する仕組みを実装した

---
+++ SwichChannelTag/Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,56 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersManager : MonoBehaviourPunCallbacks
+{
+    List<GameObject> players = new();
+
+    public GameObject[] Players { get => players.ToArray(); }
+   
+    void ResetPlayersList()
+    {
+        players.Clear();
+
+        foreach(var i in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            players.Add(i);
+        }
+    }
+
+    //プレイヤー達のComponentを配列で取得
+    public T[] GetComponentsFromPlayers<T>() where T : Component
+    {
+        List<T> ret = new();
+
+        foreach (var i in players)
+        {
+            if(i == null) continue;
+            T comp = i.GetComponent<T>();
+            if (comp != null)
+            {
+                ret.Add(comp);
+            }
+        }
+
+        return ret.ToArray();
+    }
+
+    // 入室時、他プレイヤーが入った時、他プレイヤーが抜けた時、リストを更新する
+
+    public override void OnJoinedRoom()
+    {
+        ResetPlayersList();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        ResetPlayersList();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        ResetPlayersList();
+    }
+}
