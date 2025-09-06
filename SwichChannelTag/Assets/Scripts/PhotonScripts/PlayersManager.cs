184857a63e61e12e7b6815b42d92771dfb6d5fe8
BlueAiu <aiujustitia@yahoo.co.jp>
2025-09-06 16:25:56 +0900

    プレイヤーリストの情報取得を再設計した
    GameObject,
    Photon.Realtime.Player,
    Component を配列取得できるようにした

--- SwichChannelTag/Assets/Scripts/PhotonScripts/PlayersManager.cs
+++ SwichChannelTag/Assets/Scripts/PhotonScripts/PlayersManager.cs
@@ -1,56 +1,90 @@
 using Photon.Pun;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class PlayersManager : MonoBehaviourPunCallbacks
 {
-    static List<GameObject> players = new();
+    static List<GetPlayerInfo> players = new();
 
-    public GameObject[] Players 
+
+    // --- Getter --- //
+
+    public GameObject[] PlayersGameObject
     { 
         get 
         {
-            return players.ToArray();
+            List<GameObject> ret = new();
+
+            foreach (var p in players) 
+            {
+                if(p == null) continue;
+
+                ret.Add(p.gameObject); 
+            }
+
+            return ret.ToArray();
         }
     }
 
+    public Photon.Realtime.Player[] PlayersPhotonPlayer
+    {
+        get
+        {
+            List<Photon.Realtime.Player> ret = new();
+
+            foreach (var p in players)
+            {
+                if (p == null) continue;
+
+                ret.Add(p.gameObject.GetPhotonView().Owner);
+            }
+
+            return ret.ToArray();
+        }
+    }
+
+    public T[] GetComponentsFromPlayers<T>() where T : Component
+    {
+        
+        List<T> ret = new();
+
+        foreach (var i in players)
+        {
+            if (i == null) continue;
+
+            T comp = i.GetComp<T>();
+
+            if (comp != null) { ret.Add(comp); }
+        }
+
+        return ret.ToArray();
+    }
+
+
+    // --- Add & Remove --- //
+
     public static void AddPlayer(GameObject player)
     {
-        if (players.Contains(player)) return;
-        players.Add(player);
+        var playerInfo = player.GetComponent<GetPlayerInfo>();
+
+        if (players.Contains(playerInfo)) return;
+        players.Add(playerInfo);
         SortByActorNumber();
     }
 
     public static void RemovePlayer(GameObject player)
     {
-        if (!players.Contains(player)) return;
-        players.Remove(player);
+        var playerInfo = player.GetComponent<GetPlayerInfo>();
+
+        if (!players.Contains(playerInfo)) return;
+        players.Remove(playerInfo);
         SortByActorNumber();
     }
 
     static void SortByActorNumber()
     {
         players.Sort((a, b) => 
-        a.GetPhotonView().Owner.ActorNumber.CompareTo(
-            b.GetPhotonView().Owner.ActorNumber));
-    }
-
-    //プレイヤー達のComponentを配列で取得
-    public T[] GetComponentsFromPlayers<T>() where T : Component
-    {
-        List<T> ret = new();
-
-        foreach (var i in players)
-        {
-            if(i == null) continue;
-            T comp = i.GetComponent<T>();
-            if (comp != null)
-            {
-                ret.Add(comp);
-            }
-        }
-
-        return ret.ToArray();
+        a.gameObject.GetPhotonView().Owner.ActorNumber.CompareTo(
+            b.gameObject.GetPhotonView().Owner.ActorNumber));
     }
 }
