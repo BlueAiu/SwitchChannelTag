2f650058db2ba02c6a7062a5ac2d153350815cfc
soltshio <shanshanshien@gmail.com>
2025-09-12 02:19:05 +0900

    ゲームの流れの基盤部分を作った

---
+++ SwichChannelTag/Assets/Scripts/Components/GameRule/GameFlowManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+
+
+//作成者:杉山
+//ゲームの流れを管理する
+//ステートパターンを使用予定
+
+public class GameFlowManager : MonoBehaviour
+{
+    GameFlowStateTypeBase _current;
+
+    private void Start()
+    {
+        Player mine = PlayersManager.MinePlayerPhotonPlayer;
+
+        if (!mine.IsMasterClient) return;//ホスト主以外はこの処理を行わない
+
+        StartCoroutine(GameFlow());
+    }
+
+    IEnumerator GameFlow()
+    {
+        //この時点では他のコンポーネントの初期化が終わってない可能性があるため、一旦1フレーム待つ
+        yield return null;
+
+        //開始演出のステート(実装予定)
+        
+
+        //ゲーム中は逃げる側の行動ステート→ゲーム決着判定→鬼側の行動ステート→ゲーム決着判定を繰り返す(実装予定)
+        
+
+        //終了演出のステート(実装予定)
+    }
+
+    void ChangeState(GameFlowStateTypeBase nextState)//ステートの変更
+    {
+        if(_current!=null) _current.OnExit();
+
+        _current = nextState;
+
+        if(_current!=null) _current.OnEnter();
+    }
+}
