e04d3d3f19f637d50bb0f99290a881fc66e7ffbe
soltshio <shanshanshien@gmail.com>
2025-09-27 22:44:04 +0900

    MapTransformのいらない機能の削除

--- SwichChannelTag/Assets/Scripts/Components/MapTransform/MapTransform.cs
+++ SwichChannelTag/Assets/Scripts/Components/MapTransform/MapTransform.cs
@@ -11,31 +11,14 @@ using Photon.Pun;
 public partial class MapTransform : MonoBehaviour
 {
     [Tooltip("移動できる階層一覧")][SerializeField] Maps_Hierarchies _hierarchies;
-    [Tooltip("動かす対象")][SerializeField] Transform _target;
     [Tooltip("位置")][SerializeField] MapVec _pos;
     [Tooltip("階層番号")][SerializeField] int _hierarchyIndex;
     [SerializeField] PhotonView _myPhotonView;
 
-    public Transform Target { get { return _target; } }//動かす対象
 
 
     public MapVec Pos { get { return _pos; } }//現在のマップ上の位置
     public Vector3 CurrentWorldPos { get { return CurrentHierarchy.MapToWorld(_pos); } }//現在のワールド上の位置
-
-    public void MoveSmoothly(MapVec newMapPos,float duration,bool isSync=true)//滑らかに新しいマスへ移動
-    {
-        CheckAbleToSync(ref isSync);//同期出来るかチェック(出来ないのであれば、非同期移動に変更)
-
-        //durationが_minDuration以下の場合は即時移動として処理
-        if (duration <= _minDuration)
-        {
-            Rewrite(newMapPos,isSync);
-            return;
-        }
-
-        StartMoveSmoothly(newMapPos,duration,isSync);
-    }
-    public bool Moving { get { return _moving; } }//移動中か
 
     public int HierarchyIndex { get { return _hierarchyIndex; } }//現在の階層番号
     public Map_A_Hierarchy CurrentHierarchy { get { return _hierarchies[_hierarchyIndex]; } }//現在の階層
@@ -58,7 +41,7 @@ public partial class MapTransform : MonoBehaviour
 
     public void Rewrite(MapVec newMapPos, int newHierarchyIndex, bool isSync=true)//位置と階層両方の書き換え(同期するかを決めれる)
     {
-        if (_target == null || _hierarchies == null)
+        if (_hierarchies == null)
         {
             Debug.Log("インスペクターで設定されていない箇所があるので、移動に失敗しました！");
             return;
@@ -93,8 +76,6 @@ public partial class MapTransform : MonoBehaviour
         MapVec newMapPos=new MapVec(newMapPos_X,newMqpPos_Y);
         _hierarchyIndex = newHierarchyIndex;
         _pos = newMapPos;
-        Vector3 newWorldPos = CurrentWorldPos;
-        _target.position = newWorldPos;
     }
 
     void CheckAbleToSync(ref bool isSync)
@@ -109,11 +90,6 @@ public partial class MapTransform : MonoBehaviour
     void Start()
     {
         Rewrite(_pos, _hierarchyIndex,true);
-    }
-
-    private void Update()
-    {
-        UpdateMoveSmoothly();
     }
 
     private void OnValidate()
