9dc1cea8c0f76d20628d23c5c671ded32211836c
soltshio <shanshanshien@gmail.com>
2025-09-30 12:50:23 +0900

    ずらす処理の作成(同じ位置のプレイヤーの要素番号を取得する機能以外は作成)

--- SwichChannelTag/Assets/Scripts/Components/ShiftPlayersPosition/ShiftPlayersPosition.cs
+++ SwichChannelTag/Assets/Scripts/Components/ShiftPlayersPosition/ShiftPlayersPosition.cs
@@ -14,43 +14,48 @@ public class ShiftPlayersPosition : MonoBehaviour
     GetOverlapPlayer _getOverlapPlayer;
 
     SetTransform[] _setTransforms;
+    CanShift[] _canShifts;
 
 
     public void OnExit(MapTransform myMapTrs)//マスから出ていく時(自分のマスを書き換える前に呼ぶ)
     {
-        int[] overlapPlayersIndexs= { };//重なっているプレイヤーを取得
+        int[] overlapPlayersIndexs= { };//重なっているプレイヤーを取得(☆後に番号を取得するようにする)
         Vector3 massCenterPos = myMapTrs.CurrentWorldPos;//マスの中心点を取得
-
         int offsetIndex=0;
 
-        for(int i=0; i<overlapPlayersIndexs.Length ;i++)//自分以外の同じマスのプレイヤーの位置をずらす
+        //自分以外の同じマスのプレイヤーの位置をずらす
+        for (int i=0; i<overlapPlayersIndexs.Length ;i++)
         {
             int overlapIndex=overlapPlayersIndexs[i];
 
             if (overlapIndex == PlayersManager.MyIndex) continue;//自分だったらずらさない
 
-            if(true)//それ以外の人ならずらしてもいいならずらす
+            else if (_canShifts[i].IsShiftAllowed)//それ以外の人ならずらしてもいいならずらす
             {
-                //マスの中心点にoffsetを足す
-
-                //プレイヤーの位置を動かす
+                Vector3 pos = massCenterPos + _offsets[offsetIndex];//移動位置
+                _setTransforms[i].Position = pos;
 
                 offsetIndex++;
             }
         }
     }
 
-    public void OnEnter()//マスに到着した時(自分のマスを書き換えてから呼ぶ)
+    public void OnEnter(MapTransform myMapTrs)//マスに到着した時(自分のマスを書き換えてから呼ぶ)
     {
-        int[] overlapPlayersIndex = { };//重なっているプレイヤーを取得
+        int[] overlapPlayersIndex = { };//重なっているプレイヤーを取得(☆後に番号を取得するようにする)
+        Vector3 massCenterPos = myMapTrs.CurrentWorldPos;//マスの中心点を取得
+        int offsetIndex = overlapPlayersIndex.Length-1;
 
         //自分の位置をずらす
+        Vector3 pos = massCenterPos + _offsets[offsetIndex];//移動位置
+        _setTransforms[PlayersManager.MyIndex].Position = pos;
     }
 
 
     //private
     private void Awake()
     {
+        _canShifts = PlayersManager.GetComponentsFromPlayers<CanShift>();
         _setTransforms = PlayersManager.GetComponentsFromPlayers<SetTransform>();
     }
 }
