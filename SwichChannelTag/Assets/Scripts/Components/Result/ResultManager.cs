4c98a1584c185d5c8e981e20880c2727ba083e12
soltshio <shanshanshien@gmail.com>
2025-10-07 12:47:44 +0900

    スコアを格納するクラスの作成(途中)

---
+++ SwichChannelTag/Assets/Scripts/Components/Result/ResultManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//作成者:杉山
+//結果のマネージャー(結果の確定)
+//このマネージャー自体はゲームシーンに置く
+
+public class ResultManager : MonoBehaviour
+{
+    static ScoreData _score;
+
+    public static ScoreData Score { get { return _score; } }//スコアの取得(スコアが確定していない場合はnullが返されるので注意)
+
+    //結果の確定(ゲームが終了したら結果に書き込みする)
+    public void ConfirmResult()
+    {
+
+    }
+
+    private void Awake()
+    {
+        //ゲームシーン開始時にスコア初期化
+        _score = null;
+    }
+}
