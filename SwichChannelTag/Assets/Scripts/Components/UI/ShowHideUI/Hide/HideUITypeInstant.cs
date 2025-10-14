46086d5ac2b3665b7abfe33b2ebdb9e1bfa73fa0
soltshio <shanshanshien@gmail.com>
2025-10-14 15:19:37 +0900

    UIを表示する機能の作成&ゲーム中のコマンドUIを作成

---
+++ SwichChannelTag/Assets/Scripts/Components/UI/ShowHideUI/Hide/HideUITypeInstant.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+//作成者:杉山
+//UIの非表示(すぐにパッと非表示にするタイプ)
+
+public class HideUITypeInstant : HideUITypeBase
+{
+    [Tooltip("非表示にしたいUI群")] [SerializeField]
+    CanvasGroup _hideUIGroup;
+
+    const float _hideAlpha = 0;
+
+    public override void Hide()
+    {
+        _hideUIGroup.alpha = _hideAlpha;
+        _hideUIGroup.blocksRaycasts = false;
+    }
+}
