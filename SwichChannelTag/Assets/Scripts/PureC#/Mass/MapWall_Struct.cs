e2d214ed7fd9a2dfc1f398970fd7d180a205b6d6
BlueAiu <aiujustitia@yahoo.co.jp>
2025-10-17 15:30:24 +0900

    inspectorから障害物を設置するやり方を変更した
    オブジェクト一つ一つを設置する方式から
    複数の文字列を入力してマップに設置する方式にした

--- SwichChannelTag/Assets/Scripts/PureC#/Mass/MapWall_Struct.cs
+++ SwichChannelTag/Assets/Scripts/PureC#/Mass/MapWall_Struct.cs
@@ -6,4 +6,18 @@ public struct MapWall_Obstacle
     public int hierarchyIndex;
     public MapVec pos;
     public bool blockUnder;     // カメラから見てマスの下側または右側に配置する
+
+    public MapWall_Obstacle(int hierarchyIndex, MapVec pos, bool blockUnder)
+    {
+        this.hierarchyIndex = hierarchyIndex;
+        this.pos = pos;
+        this.blockUnder = blockUnder;
+    }
+
+    public MapWall_Obstacle(MapPos mapPos, bool blockUnder)
+    {
+        this.hierarchyIndex = mapPos.hierarchyIndex;
+        this.pos = mapPos.gridPos;
+        this.blockUnder = blockUnder;
+    }
 }
