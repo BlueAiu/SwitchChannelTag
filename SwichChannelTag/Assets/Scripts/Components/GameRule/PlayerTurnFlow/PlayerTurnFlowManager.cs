ea3d966c7713acbbef0d6556c4bdb52cfb920fde
soltshio <shanshanshien@gmail.com>
2025-10-20 13:10:18 +0900

    レビュー個所を修正

--- SwichChannelTag/Assets/Scripts/Components/GameRule/PlayerTurnFlow/PlayerTurnFlowManager.cs
+++ SwichChannelTag/Assets/Scripts/Components/GameRule/PlayerTurnFlow/PlayerTurnFlowManager.cs
@@ -7,26 +7,8 @@ using UnityEngine;
 public class PlayerTurnFlowManager : MonoBehaviour
 {
     //--- ステート関係 ---//
-
-    [Tooltip("行動選択ステート")] [SerializeField]
-    PlayerTurnFlowStateTypeSelectAction _selectActionState;
-
-    [Tooltip("ダイスステート")] [SerializeField]
-    PlayerTurnFlowStateTypeDice _diceState;
-
-    [Tooltip("移動ステート")] [SerializeField]
-    PlayerTurnFlowStateTypeMove _moveState;
-
-    [Tooltip("階層選択ステート")] [SerializeField]
-    PlayerTurnFlowStateTypeSelectHierarchy _selectHierarchyState;
-
-    [Tooltip("階層移動ステート")] [SerializeField]
-    PlayerTurnFlowStateTypeChangeHierarchy _changeHierarchyState;
-
-    [Tooltip("行動終了ステート")] [SerializeField]
-    PlayerTurnFlowStateTypeFinish _finishState;
-
-    Dictionary<EPlayerTurnState, PlayerTurnFlowStateTypeBase> _playerTurnStateDic=new Dictionary<EPlayerTurnState, PlayerTurnFlowStateTypeBase>();
+    [Tooltip("プレイヤーの行動ステート")] [SerializeField]
+    SerializableDictionary<EPlayerTurnState, PlayerTurnFlowStateTypeBase> _playerTurnStateDic;
 
     EPlayerTurnState _nowEState=EPlayerTurnState.None;
     EPlayerTurnState _beforeEState=EPlayerTurnState.None;
@@ -43,14 +25,6 @@ public class PlayerTurnFlowManager : MonoBehaviour
         _myTurnIsReady = PlayersManager.GetComponentFromMinePlayer<TurnIsReady>();
 
         _myTurnIsReady.OnStartTurn += StartMyTurn;
-
-        //辞書にステートを登録
-        _playerTurnStateDic.Add(EPlayerTurnState.SelectAction, _selectActionState);
-        _playerTurnStateDic.Add(EPlayerTurnState.Dice, _diceState);
-        _playerTurnStateDic.Add(EPlayerTurnState.Move, _moveState);
-        _playerTurnStateDic.Add(EPlayerTurnState.SelectHierarchy, _selectHierarchyState);
-        _playerTurnStateDic.Add(EPlayerTurnState.ChangeHierarchy, _changeHierarchyState);
-        _playerTurnStateDic.Add(EPlayerTurnState.Finish, _finishState);
     }
 
     void StartMyTurn()//自分の行動の許可が出た時に呼び出す
