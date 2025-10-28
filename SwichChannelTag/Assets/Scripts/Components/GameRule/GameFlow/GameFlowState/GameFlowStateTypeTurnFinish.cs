250e2c59d0908da685a8579fa6297c7d40690d59
soltshio <shanshanshien@gmail.com>
2025-10-28 12:56:58 +0900

    お互いのターンが終了した時のステートを作成

---
+++ SwichChannelTag/Assets/Scripts/Components/GameRule/GameFlow/GameFlowState/GameFlowStateTypeTurnFinish.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//作成者:杉山
+//お互いのターン終了時のステート
+
+public class GameFlowStateTypeTurnFinish : GameFlowStateTypeBase
+{
+    public override void OnEnter()
+    {
+        //ここで経過ターンを増やす
+    }
+
+    public override void OnUpdate()
+    {
+        //指定ターン経過したらゲーム終了させる
+        //そうでなければプレイヤーにターンを回す
+
+        EGameState nextState = (_stateMachine.SharedData.FirstTurn == EPlayerState.Runner) ? EGameState.RunnerTurn : EGameState.TaggerTurn;
+
+        _stateMachine.ChangeState(nextState);
+    }
+
+    public override void OnExit()
+    {
+
+    }
+}
