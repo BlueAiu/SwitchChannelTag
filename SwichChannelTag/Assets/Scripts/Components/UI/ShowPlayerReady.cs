abdc4d517ba2a8fd22097f790a77ca7d6ff05dcc
Kim-game3 <zweilous387@icloud.com>
2025-11-18 13:33:33 +0900

    一時保存

---
+++ SwichChannelTag/Assets/Scripts/Components/UI/ShowPlayerReady.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPlayerReady : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject ReadyText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
