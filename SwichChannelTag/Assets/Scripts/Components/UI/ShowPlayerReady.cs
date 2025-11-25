7f6d241879efc87b69346a60380777cfbd25d44d
Kim-game3 <zweilous387@icloud.com>
2025-11-25 14:33:36 +0900

    一時保存

--- SwichChannelTag/Assets/Scripts/Components/UI/ShowPlayerReady.cs
+++ SwichChannelTag/Assets/Scripts/Components/UI/ShowPlayerReady.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class ShowPlayerReady : MonoBehaviourPunCallbacks
 {
     [SerializeField] GameObject[] Ready_Massage;
+
+    private int PlayerNum;
     // Start is called before the first frame update
 
-    public override void OnJoinedRoom()
+    private void Start()
     {
         for(int i = 0; i < Ready_Massage.Length; i++)
         {
@@ -16,5 +18,19 @@ public class ShowPlayerReady : MonoBehaviourPunCallbacks
         }
     }
 
-
+    public void ShowReady()
+    {
+        PlayerNum = photonView.OwnerActorNr - 1;
+        switch(PlayerNum)
+        {
+            case 0:
+                Ready_Massage[0].SetActive(true); break;
+            case 1:
+                Ready_Massage[1].SetActive(true); break;
+            case 2:
+                Ready_Massage[2].SetActive(true); break;
+            case 3:
+                Ready_Massage[3].SetActive(true); break;
+        }
+    }
 }
