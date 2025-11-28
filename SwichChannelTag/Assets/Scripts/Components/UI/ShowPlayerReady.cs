d4104a69e48405661894ec1eee9a434e999b9695
Kim-game3 <zweilous387@icloud.com>
2025-11-28 11:05:42 +0900

    Readyを押したかどうかが他のプレイヤーにもわかるようにしている
    Indexに使うものをPlayersManagerのMyIndexから別の変数に変更しました。後できていないのはReadyを押したまま退室した時の処理等の入退室時の処理です。

--- SwichChannelTag/Assets/Scripts/Components/UI/ShowPlayerReady.cs
+++ SwichChannelTag/Assets/Scripts/Components/UI/ShowPlayerReady.cs
@@ -1,36 +1,123 @@
+using JetBrains.Annotations;
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ShowPlayerReady : MonoBehaviourPunCallbacks
 {
     [SerializeField] GameObject[] Ready_Massage;
 
-    private int PlayerNum;
-    // Start is called before the first frame update
+    private int Player_Number;
+    private int index;
+    private int Left_count = 0;
 
     private void Start()
     {
+        if(Ready_Massage == null)
+        {
+            Debug.LogError("The text is not attached.");
+            return;
+        }
         for(int i = 0; i < Ready_Massage.Length; i++)
         {
             Ready_Massage[i].SetActive(false);
         }
+
+        UpdatePlayerNum();
     }
 
     public void ShowReady()
     {
-        PlayerNum = photonView.OwnerActorNr - 1;
-        switch(PlayerNum)
+        if(Player_Number < 0)
         {
-            case 0:
-                Ready_Massage[0].SetActive(true); break;
-            case 1:
-                Ready_Massage[1].SetActive(true); break;
-            case 2:
-                Ready_Massage[2].SetActive(true); break;
-            case 3:
-                Ready_Massage[3].SetActive(true); break;
+            Debug.LogError("the value is null");
+            return;
+        }
+
+        //index = PlayersManager.MyIndex;
+        index = Left_count;
+
+        Debug.Log("Player Index: " + index);
+
+        var readyText = Ready_Massage[index];
+
+        bool isActive = !readyText.activeSelf;
+        photonView.RPC(nameof(RPC_ShowReady), RpcTarget.AllBuffered, index, isActive);
+        Debug.Log($"ShowReady: player {index} -> {(isActive ? "shown" : "hidden")}");
+    }
+
+    [PunRPC]
+    private void RPC_ShowReady(int index, bool active)
+    {
+       var readyText = Ready_Massage[index];
+
+        readyText.SetActive(active);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        UpdatePlayerNum();
+    }
+
+    public override void OnLeftRoom()
+    {
+        if (Ready_Massage[index].activeSelf)
+        {
+            photonView.RPC(nameof(RPC_ShowReady), RpcTarget.AllBuffered, index, false);
+            Left_count = 0;
+        }
+        else
+        {
+            Left_count = 0;
         }
     }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log("index: " + index);
+        if (Ready_Massage[index].activeSelf)
+        {
+            photonView.RPC(nameof(RPC_ShowReady), RpcTarget.AllBuffered, index, false);
+            for (int i = 0; i < Ready_Massage.Length; i++)
+            {
+                Ready_Massage[i].SetActive(false);
+            }
+            UpdatePlayerNum();
+            //Ready_Massage[Left_count].SetActive(true);
+            Debug.Log("Left_count: " + Left_count);
+            photonView.RPC(nameof(RPC_ShowReady), RpcTarget.AllBuffered, Left_count, true);
+        }
+        else
+        {
+            UpdatePlayerNum();
+        }
+        
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        UpdatePlayerNum();
+    }
+
+    //自分より先にルーム内にいるプレイヤーをカウントする関数
+    private void UpdatePlayerNum()
+    {
+        if(!PhotonNetwork.InRoom || PhotonNetwork.LocalPlayer == null)
+        {
+            Left_count = -1;
+            Debug.Log("UpdatePlayerNum: Not in room or local player is null.");
+            return;
+        }
+
+        int myActor = PhotonNetwork.LocalPlayer.ActorNumber;
+
+        Left_count = PhotonNetwork.PlayerList.Count(p => p.ActorNumber < myActor);
+
+        index = Left_count;
+    }
+
+    
 }
