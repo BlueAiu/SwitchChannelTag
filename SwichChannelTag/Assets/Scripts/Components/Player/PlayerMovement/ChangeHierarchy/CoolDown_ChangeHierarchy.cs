6c1300347f0eac1d477284d3ae7aed109b2b29c2
BlueAiu <aiujustitia@yahoo.co.jp>
2025-12-23 11:14:56 +0900

    一人の時にブーストを行うテストプレイ

--- SwichChannelTag/Assets/Scripts/Components/Player/PlayerMovement/ChangeHierarchy/CoolDown_ChangeHierarchy.cs
+++ SwichChannelTag/Assets/Scripts/Components/Player/PlayerMovement/ChangeHierarchy/CoolDown_ChangeHierarchy.cs
@@ -6,6 +6,7 @@ public class CoolDown_ChangeHierarchy : MonoBehaviour
 {
     [SerializeField] SerializableDictionary<EPlayerState, int> cooldowns;
     [SerializeField] ChangeHierarchy _changeHierarchy;
+    [SerializeField] int lonelyBoost = 1;
     PlayerState _myState;
 
     const int past = int.MinValue / 2;
@@ -13,7 +14,8 @@ public class CoolDown_ChangeHierarchy : MonoBehaviour
 
     public int CoolDownLeft
     {
-        get => Mathf.Max(0, cooldowns[_myState.State] - (GameStatsManager.Instance.Turn.GetTurn() - lastChangedHierarckyTurn));
+        get => Mathf.Max(0, cooldowns[_myState.State] - (GameStatsManager.Instance.Turn.GetTurn() - lastChangedHierarckyTurn) 
+            - (Islonely() ? lonelyBoost : 0));
     }
 
     public bool CanChangeHierarchy
@@ -30,5 +32,16 @@ public class CoolDown_ChangeHierarchy : MonoBehaviour
     {
         _myState = PlayersManager.GetComponentFromMinePlayer<PlayerState>();
         _changeHierarchy.OnSwitchHierarchy+= SetLastChangedTurn;//ŠK‘wˆÚ“®‚ÉÅŒã‚ÉŠK‘wˆÚ“®‚ğ‚µ‚½ƒ^[ƒ“‚ğ‹L˜^
+    }
+
+    bool Islonely()
+    {
+        var palyers = PlayersManager.GetComponentsFromPlayers<PlayerState>();
+        int cnt = 0;
+        foreach(var player in palyers)
+        {
+            if (player.State == EPlayerState.Runner) cnt++;
+        }
+        return cnt == 1;
     }
 }
--- SwichChannelTag/Assets/Scripts/Components/Player/PlayerMovement/DecideMovableStep/DecideMovableStep.cs
+++ SwichChannelTag/Assets/Scripts/Components/Player/PlayerMovement/DecideMovableStep/DecideMovableStep.cs
@@ -20,6 +20,8 @@ public class DecideMovableStep : MonoBehaviour
 
     [SerializeField] bool writeLog = true;
 
+    [SerializeField] SerializableDictionary<EPlayerState, int> lonelyBoost;
+
     public void Decide(bool isChangedHierarchy)//動けるマス数を決定(ダイスロールで)、階層移動したかを受け取る
     {
         int result;
@@ -51,6 +53,10 @@ public class DecideMovableStep : MonoBehaviour
                 result = 0;
             }
         }
+        if (IsLonely())
+        {
+            result += lonelyBoost[_playerState.State];
+        }
 
         _decidePath.RemainingStep=result;
     }
@@ -66,5 +72,16 @@ public class DecideMovableStep : MonoBehaviour
         {
             _playerState = PlayersManager.GetComponentFromMinePlayer<PlayerState>();
         }
+    }
+
+    bool IsLonely()
+    {
+        var players = PlayersManager.GetComponentsFromPlayers<PlayerState>();
+        int cnt = 0;
+        foreach(var player in players)
+        {
+            if(player.State == _playerState.State) cnt++;
+        }
+        return cnt == 1;
     }
 }
