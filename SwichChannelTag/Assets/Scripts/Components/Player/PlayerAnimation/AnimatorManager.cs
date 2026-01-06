0b60172c84ec1cc30e20058bb7860419f9048949
soltshio <shanshanshien@gmail.com>
2026-01-06 17:10:43 +0900

    アニメーションを同期させる仕組みを作成

--- SwichChannelTag/Assets/Scripts/Components/Player/PlayerAnimation/AnimatorManager.cs
+++ SwichChannelTag/Assets/Scripts/Components/Player/PlayerAnimation/AnimatorManager.cs
@@ -1,36 +1,40 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class AnimatorManager : MonoBehaviour
 {
     [SerializeField] SerializableDictionary<EPlayerState, Animator> _animator;
+    [SerializeField] PhotonView _myPhotonView;
     [SerializeField] PlayerState _state;
 
-    Animator CurAnim { get => _animator[_state.State]; }
+    public void SetBool(string name,bool value)
+    {
+        _myPhotonView.RPC(nameof(SetBool_RPC), RpcTarget.All, name, value);
+    }
 
-    const string captureTrigger = "CaptureTrigger";
-    const string isMoving = "IsMoving";
-    const string switchTrigger = "SwitchTrigger";
+    public void SetTrigger(string name)
+    {
+        _myPhotonView.RPC(nameof(SetTrigger_RPC), RpcTarget.All, name);
+    }
+
+    [PunRPC]
+    void SetBool_RPC(string name, bool value)
+    {
+        if (!_animator.TryGetValue(_state.State, out var animator)) return;
+
+        animator.SetBool(name, value);
+    }
+
+    [PunRPC]
+    void SetTrigger_RPC(string name)
+    {
+        if (!_animator.TryGetValue(_state.State, out var animator)) return;
+
+        animator.SetTrigger(name);
+    }
+
     
-
-    public void StartMove()
-    {
-        CurAnim.SetBool(isMoving, true);
-    }
-
-    public void EndMove()
-    {
-        CurAnim.SetBool(isMoving, false);
-    }
-
-    public void CaptureTrigger()
-    {
-        CurAnim.SetTrigger(captureTrigger);
-    }
-
-    public void SwitchTrigger()
-    {
-        CurAnim.SetTrigger(switchTrigger);
-    }
 }
