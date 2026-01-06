040bdba80f9c13bd3c4bdc4ea592a05212b490d0
BlueAiu <aiujustitia@yahoo.co.jp>
2026-01-06 13:59:06 +0900

    残りターンが変化しない不具合を修正した

--- SwichChannelTag/Assets/Scripts/Components/UI/TurnNumUI.cs
+++ SwichChannelTag/Assets/Scripts/Components/UI/TurnNumUI.cs
@@ -6,7 +6,7 @@ public class TurnNumUI : MonoBehaviour
     [SerializeField] TMP_Text _text;
     [SerializeField] MaxTurnNumConfig _maxTurn;
 
-    void FixtdUpdate()
+    private void FixedUpdate()
     {
         _text.text = (_maxTurn.MaxTurnNum - GameStatsManager.Instance.Turn.GetTurn() + 1).ToString();
     }
