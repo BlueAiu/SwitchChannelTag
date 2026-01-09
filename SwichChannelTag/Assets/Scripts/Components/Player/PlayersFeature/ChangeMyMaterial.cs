c5efb8951d60f42424caf94951735f41177a526c
soltshio <shanshanshien@gmail.com>
2026-01-10 08:10:17 +0900

    プレイヤーの特徴の設定をした

---
+++ SwichChannelTag/Assets/Scripts/Components/Player/PlayersFeature/ChangeMyMaterial.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//作成者:杉山
+//自分のプレイヤーの色(マテリアル)を変える
+
+public class ChangeMyMaterial : MonoBehaviour
+{
+    [Tooltip("鬼のスキン")]
+    [SerializeField]
+    Renderer[] _taggerSkins;
+
+    [Tooltip("逃げのスキン")]
+    [SerializeField]
+    Renderer[] _runnerSkins;
+
+    Material[] _taggerMats;
+    Material[] _runnerMats;
+
+    private void Awake()
+    {
+        _taggerMats = new Material[_taggerSkins.Length];
+        for (int i = 0; i < _taggerSkins.Length; i++)
+        {
+            _taggerMats[i] = _taggerSkins[i].material;
+        }
+
+        _runnerMats = new Material[_runnerSkins.Length];
+        for (int i = 0; i < _runnerSkins.Length; i++)
+        {
+            _runnerMats[i] = _runnerSkins[i].material;
+        }
+    }
+
+    public void SetColorMaterials(Material[] taggerMaterials, Material[] runnerMaterials)
+    {
+        //// 鬼の色を変更
+        //foreach (var mat in _taggerMats)
+        //{
+        //    mat.color = color;
+        //}
+
+        //// 逃げの色を変更
+        //foreach (var mat in _runnerMats)
+        //{
+        //    mat.color = color;
+        //    mat.SetColor("_EmissionColor", color);
+        //}
+    }
+}
